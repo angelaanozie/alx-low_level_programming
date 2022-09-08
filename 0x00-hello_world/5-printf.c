c649479f2b0ec3cdd095639a7fb7917dd33e75be
angelaanozie <angelaanozie.c@gmail.com>
2022-09-08 16:50:54 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 /**
- *
- * * main - Prints a text
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+ *  * main - print the string in the put function
+ *   *
+ *    * Description: using the main fuction
+ *     * with proper grammar, but the outcome is a piece of art
+ *      * Return: 0
+ *      */
 int main(void)
-
 {
-
-printf("with proper grammar, but the outcome is a piece of 
-
-art,\n");
-
-return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
