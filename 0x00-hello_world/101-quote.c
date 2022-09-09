2bbec5cda087dd4adcde9d1cfafb537f49738632
angelaanozie <angelaanozie.c@gmail.com>
2022-09-08 18:53:28 -0700

    Task 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  * Description: using the main fuction
  * prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
  * prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
- */
+*/
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
