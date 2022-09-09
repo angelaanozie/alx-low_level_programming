42b79a489d0a14fc50961d5477f9ff1bee40c81c
angelaanozie <angelaanozie.c@gmail.com>
2022-09-08 20:01:06 -0700

    Task 6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - print the string in the put function
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
@@ -10,7 +9,7 @@ int a;
 long int b;
 long long int c;
 char d;
-float e;
+float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
