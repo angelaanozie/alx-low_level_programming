feb45c5ffe8a802af297e302c896c2979ce1abb4
angelaanozie <angelaanozie.c@gmail.com>
2022-09-21 18:44:30 -0700

    Task 9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * print_number - takes an integer and prints it with with _putchar
- * @n: integer to print
- * Return: void
+ * print_number - Prints an integer
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
@@ -13,7 +12,9 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
+
 	if ((num / 10) > 0)
 		print_number(num / 10);
+
 	_putchar((num % 10) + '0');
 }
